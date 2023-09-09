858f5ef694b608edc06f5ce743f57ad46231d9fc
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-09-09 16:09:41 +0300

    goit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './js/api';
+import { createMarkup, createMarkupCat } from './js/markup';
+import refs from './js/refs';
+
+refs.selectEl.addEventListener('change', onValueId);
+fetchBreeds()
+  .then(arr => {
+    load();
+
+    return (refs.selectEl.innerHTML = createMarkup(arr.data));
+  })
+  .then(() => slim())
+  .catch(fetchError);
+
+function onValueId(e) {
+  const id = e.target.value;
+  fetchCatByBreed(id)
+    .then(obj => {
+      load();
+
+      return (refs.catInfoEl.innerHTML = createMarkupCat(obj.data));
+    })
+    .then(() => success())
+    .catch(fetchError);
+}
+function fetchError() {
+  Report.failure(refs.error.textContent, '');
+}
+function success() {
+  Notify.success('Search was successful!)', '');
+}
+function load() {
+  refs.selectEl.hidden = false;
+  refs.loaderEl.classList.remove('loader');
+}
+
+function slim() {
+  new SlimSelect({
+    select: refs.selectEl,
+  });
+}
